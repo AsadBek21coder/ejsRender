bb3808ba38c732e5744bfadb1f358129d05eeb5c
AsadBek21coder <asadbekegambergenoov21coder@gmail.com>
2020-08-08 16:57:01 +0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,18 @@ const app = express()
 const port = 8080;
 // const cors = reqiure('cors')
 // app.use(cors())
-
+function getHomePage (req,res) {
+	res.send ('hello')
+}
+function getCars (req,res) {
+	res.send('this is Cars page')
+}
+function getFlowers (req,res) {
+	res.send('this is Flowers page')
+}
+app.get('/', getHomePage)
+app.get('/cars', getCars)
+app.get('/flowers', getFlowers)
 
 
 
