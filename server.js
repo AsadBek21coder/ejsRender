9d0440f2f65aa67c95bd0ec5b76977370d7658f8
AsadBek21coder <asadbekegambergenoov21coder@gmail.com>
2020-08-08 16:52:31 +0500

    localhot 8080

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 8080;
+// const cors = reqiure('cors')
+// app.use(cors())
+
+
+
+
+app.listen(port, () => console.log(`Server is running at${port}`))
